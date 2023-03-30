a3b89409a3474c38a8e74555f59cc1a1b48842cd
andres.jimenez <ancjimenezm@correo.udistrital.edu.co>
2023-03-30 03:05:57 -0500

    cambios 2

--- ProyectoVentasMultinivel/src/main/java/com/udistrital/app/repository/PeriodoRepository.java
+++ ProyectoVentasMultinivel/src/main/java/com/udistrital/app/repository/PeriodoRepository.java
@@ -1,14 +1,9 @@
 package com.udistrital.app.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.udistrital.app.entity.Categoria;
 import com.udistrital.app.entity.Periodo;
-import com.udistrital.app.entity.dto.CategoriaDto;
 
 public interface PeriodoRepository extends JpaRepository<Periodo, String> {
 
